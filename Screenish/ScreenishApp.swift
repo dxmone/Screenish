1413f0a9e6c3e6281f1bd1617c33461e65a4b230
Thomas Strömberg <thomas.stromberg@gmail.com>
2026-06-15 08:04:41 +0200

    Initial Commit

---
+++ Screenish/ScreenishApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenishApp.swift
+//  Screenish
+//
+//  Created by Thomas Strömberg on 2026-06-15.
+//
+
+import SwiftUI
+
+@main
+struct ScreenishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
